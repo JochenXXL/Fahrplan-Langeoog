2e6ed017c4814aff3aebca21ea6f158c3d32dc00
Sven Harder <info@i-modich.de>
2016-06-28 19:06:52 +0200

    Added index file for bootstrap

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,8 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo $site_title ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php if($site_description != null) {echo $site_description;}?>">
@@ -10,11 +12,9 @@
         <link href='http://fonts.googleapis.com/css?family=Raleway:800|Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="css/responsive.css">
-        <link rel="stylesheet" href="jquery/jquery-ui.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <script src="jquery/external/jquery/jquery.js"></script>
-        <script src="jquery/jquery-ui.js"></script>
-        <script type="text/javascript" src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/minified/i18n/jquery-ui-i18n.min.js"></script>
+
+
         <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
         <script type="text/javascript">
           window.cookieconsent_options = {"message":"Diese Webseite verwendet Cookies, um Ihnen ein optimales Surf Erlebnis zu gewährleisten","dismiss":"Ay, ay, Käptn","learnMore":"Mehr Informationen","link":"http://fahrplan-langeoog.de/datenschutz.php","theme":"dark-bottom"};
@@ -22,10 +22,15 @@
 
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
         <!-- End Cookie Consent plugin -->
+
     </head>
     <body>
         <?php include_once("inc/analyticstracking.php") ?>
-        <header>
+
+
+            
+
+            <header>
             <div id="header-wrapper">
                 <a href="http://fahrplan-langeoog.de" id="logo">
                 <table>
@@ -44,4 +49,5 @@
                 </nav>
             </div>
         </header>
+    
     </body>
---
+++ index2.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>     
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Linie <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Langeoog</a></li>
+                  <li><a href="#">Bensersiel</a></li>
+                  <li><a href="#">Preise</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Charter</a></li>
+              <li><a href="#">Ausflüge</a></li>
+              <li><a href="#">My-Langeoog</a></li>      
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+
+    <div class="container">
+      <div class="row">
+
+      <!-- END HEADER -->
+
+        <!-- Bild Startseite -->
+        <div class="jumbotron">
+          <h1>Bild der Fähre</h1>
+          <p>Abfahrtszeiten der Fähre Langeoog</p>
+        </div>
+
+        <!-- Grid Startseite -->
+
+        <!-- Content -->
+        <div class="col col-sm-9">
+        
+        <div class="row">
+          <h2 class="text-center"> Top ADD </h2>
+        </div>
+
+        <h1 class="text-center">Aktuelle Abfahrtszeiten ( Linie )</h1>
+        <div class="row">
+          <div class="well col col-sm-10 col-sm-offset-1">
+            <div class="col col-sm-6">
+              Fahrplan Langeoog
+            </div>
+            <div class="col col-sm-6">
+              Fahrplan Bensersiel
+            </div>
+          </div>
+        </div>
+        <div class="alert alert-info col col-sm-10 col-sm-offset-1" role="alert">
+          Achtung, alle Angaben sind ohne Gewähr!!!!
+        </div>
+
+        <h1 class="text-center">Charterfahrten</h1>
+        <div class="row">
+          <div class="well col col-sm-10 col-sm-offset-1">
+            Infotext
+          </div>
+        </div>
+
+        <h1 class="text-center">Ausflugsfahrten</h1>
+        <div class="row">
+          <div class="well col col-sm-10 col-sm-offset-1">
+            Infotext
+          </div>
+        </div>
+
+         <div class="row">
+          <h2 class="text-center"> Bottom ADD </h2>
+        </div>
+
+
+        </div>
+        <!-- END Content -->
+
+        <!-- Add rechts -->
+        <div class="col col-sm-3">
+          ADD
+        </div>
+        <!-- END Add rechts -->
+
+
+
+
+
+      <!-- FOOTER BEGIN -->
+      </div>
+
+    </div> 
+
+    <div class="container-fluid">
+      <div class="row">
+       footer 
+      </div>
+    </div> 
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
