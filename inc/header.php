769375655ef5b747eeb9cf88559283438dd724c5
Sven Harder <info@i-modich.de>
2016-08-18 15:40:53 +0200

    added all auflüge ms flinthörn

--- inc/header.php
+++ inc/header.php
@@ -18,16 +18,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
-
-    <script type="text/javascript">
-      window.cookieconsent_options = {"message":"Diese Webseite verwendet Cookies, um Ihnen ein optimales Surf Erlebnis zu gewährleisten","dismiss":"Ay, ay, Käptn","learnMore":"Mehr Informationen","link":"http://fahrplan-langeoog.de/datenschutz.php","theme":"dark-bottom"};
-    </script>
-
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
-    <!-- End Cookie Consent plugin -->
     <?php include_once("inc/functions.php"); ?>
-
   </head>
   <body> 
     <!-- Navigation -->    
